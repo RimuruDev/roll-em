9120497f6e2ea3fecd36a195ceae42196328238c
RimuruDev <Rimuru.Dev@gmail.com>
2025-11-21 22:28:43 +0400

    Added AsmDef for Win native

--- Assets/Scripts/Window/DraggableWindow.cs
+++ Assets/Scripts/Window/DraggableWindow.cs
@@ -1,46 +1,53 @@
+#if (UNITY_STANDALONE_WIN && !UNITY_EDITOR)
+#define RIM_DEF_WIN_API
+#endif
+
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
+
+#if RIM_DEF_WIN_API
+using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+#endif
 
 public class DraggableWindow : MonoBehaviour, IPointerDownHandler
 {
-    [DllImport("user32.dll")]
-    public static extern int SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
-    [DllImport("user32.dll")]
-    public static extern bool ReleaseCapture();
-
+#if RIM_DEF_WIN_API
     private const int WM_NCLBUTTONDOWN = 0xA1;
     private const int HT_CAPTION = 0x2;
 
-    private RectTransform dragArea; // UI элемент, за который перетаскиваем
+    private RectTransform dragArea;
+#endif
 
-    private System.IntPtr windowHandle; // Хэндл окна
+#if RIM_DEF_WIN_API
+    [DllImport("user32.dll")]
+    private static extern int SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+    [DllImport("user32.dll")]
+    private static extern bool ReleaseCapture();
+
+    private IntPtr windowHandle;
+#endif
 
     private void Awake()
     {
+#if RIM_DEF_WIN_API
         dragArea = GetComponent<RectTransform>();
-    }
-
-    void Start()
-    {
-        // Получаем хэндл окна в Start, чтобы не делать это каждый кадр
-        windowHandle = GetHandle();
+        
+        var process = Process.GetCurrentProcess();
+        windowHandle = process.MainWindowHandle;
+#endif
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+#if RIM_DEF_WIN_API
         if (dragArea != null && RectTransformUtility.RectangleContainsScreenPoint(dragArea, eventData.position, Camera.main))
         {
             ReleaseCapture();
             SendMessage(windowHandle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
         }
-    }
-
-    // Получение хэндла окна
-    private System.IntPtr GetHandle()
-    {
-        System.Diagnostics.Process p = System.Diagnostics.Process.GetCurrentProcess();
-        return p.MainWindowHandle;
+#endif
     }
 }
