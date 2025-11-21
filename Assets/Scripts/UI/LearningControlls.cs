ada2aeb29df7f2a1438e4a95efbed241b48b810b
RimuruDev <Rimuru.Dev@gmail.com>
2025-11-22 02:24:17 +0400

    Fixed education UX 2/2

--- Assets/Scripts/UI/LearningControlls.cs
+++ Assets/Scripts/UI/LearningControlls.cs
@@ -1,7 +1,46 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+
+// NOTE: Не совсем понимаю где утилиты Фокси хранит, по этому просто оставлю тут.
+// Очень больно не использовать var... но нужно придерживаться кодстайла фокса, раз проект так написан, держим стиль... 
+public static class CanvasGroupFadeUtility
+{
+    public static IEnumerator FadeIn(CanvasGroup canvasGroup, float duration)
+    {
+        if (canvasGroup == null)
+            yield break;
+
+        if (duration <= 0f)
+        {
+            canvasGroup.alpha = 1f;
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+            
+            yield break;
+        }
+
+        float elapsed = 0f;
+        float startAlpha = canvasGroup.alpha;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            
+            float t = elapsed / duration;
+            
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, 1f, t);
+            
+            yield return null;
+        }
+
+        canvasGroup.alpha = 1f;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
+}
 
 public class LearningControlls : MonoBehaviour
 {
@@ -14,6 +53,10 @@ public class LearningControlls : MonoBehaviour
 
     [SerializeField] private bool _mirrorFirstHint = true;
 
+    [SerializeField] private bool _useFadeForUiGroups = true;
+    [SerializeField] private CanvasGroup[] _uiGroupsForFade;
+    [SerializeField] private float _fadeDurationSeconds = 1.5f;
+
     private int _hint;
 
     private void Awake()
@@ -22,6 +65,9 @@ public class LearningControlls : MonoBehaviour
         
         if (PlayerData.showLearning)
         {
+            if (_useFadeForUiGroups)
+                InitializeUiGroupsForLearning();
+            
             Links.gameManager.PauseTime();
             ShowNextHint();
         }
@@ -42,21 +88,60 @@ public class LearningControlls : MonoBehaviour
             // NOTE: зеркалим только первый слайд, если включено
             // Так как по умолчанию в обучении раскладка отрисована не корректно, сбивает с толку. 
             // Возможно это конечно задумка автора. Но пох, это же форк, можно и пошалить.
-            var scale = _demoImage.rectTransform.localScale;
+            Vector3 scale = _demoImage.rectTransform.localScale;
             if (_hint == 0 && _mirrorFirstHint)
                 scale.x = -Mathf.Abs(scale.x);
             else
                 scale.x = Mathf.Abs(scale.x);
             _demoImage.rectTransform.localScale = scale;
 
+            UiImageScaleAnimator animator = _demoImage.GetComponent<UiImageScaleAnimator>();
+            if (animator != null)
+                animator.Play();
+
             _hint++;
         }
         else
         {
+            if (_useFadeForUiGroups)
+                StartFadeInUiGroups();
+
             OnLearningEnded?.Invoke();
             PlayerData.showLearning = false;
             Links.gameManager.CancelPauseTime();
             gameObject.SetActive(false);
+        }
+    }
+
+    private void InitializeUiGroupsForLearning()
+    {
+        if (_uiGroupsForFade == null)
+            return;
+
+        for (int i = 0; i < _uiGroupsForFade.Length; i++)
+        {
+            CanvasGroup group = _uiGroupsForFade[i];
+            if (group == null)
+                continue;
+
+            group.alpha = 0f;
+            group.interactable = false;
+            group.blocksRaycasts = false;
+        }
+    }
+
+    private void StartFadeInUiGroups()
+    {
+        if (_uiGroupsForFade == null)
+            return;
+
+        for (int i = 0; i < _uiGroupsForFade.Length; i++)
+        {
+            CanvasGroup group = _uiGroupsForFade[i];
+            if (group == null)
+                continue;
+
+            Links.gameManager.StartCoroutine(CanvasGroupFadeUtility.FadeIn(group, _fadeDurationSeconds));
         }
     }
 }
---
+++ Assets/Scripts/_Common/UiImageScaleAnimator.cs
@@ -0,0 +1,84 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Scripting;
+
+[Preserve]
+[SelectionBase]
+[DisallowMultipleComponent]
+public sealed class UiImageScaleAnimator : MonoBehaviour
+{
+    [SerializeField] private RectTransform _target;
+    [SerializeField] private float _duration = 0.25f;
+    [SerializeField] private float _startScale = 0.9f;
+    [SerializeField] private float _endScale = 1f;
+    [SerializeField] private bool _useUnscaledTime = true;
+    [SerializeField] private bool _playOnEnable;
+
+    private Coroutine _routine;
+    private Vector3 _baseScale;
+
+    private void OnValidate()
+    {
+        if (_target == null)
+            _target = GetComponent<RectTransform>();
+    }
+
+    private void Awake()
+    {
+        if (_target == null)
+            _target = GetComponent<RectTransform>();
+
+        if (_target != null)
+            _baseScale = _target.localScale;
+    }
+
+    private void OnEnable()
+    {
+        if (_playOnEnable)
+            Play();
+    }
+
+    private void OnDisable()
+    {
+        if (_routine != null)
+            StopCoroutine(_routine);
+
+        if (_target != null)
+            _target.localScale = _baseScale;
+    }
+
+    public void Play()
+    {
+        if (_target == null)
+            return;
+
+        if (_routine != null)
+            StopCoroutine(_routine);
+
+        _routine = StartCoroutine(PlayRoutine());
+    }
+
+    private IEnumerator PlayRoutine()
+    {
+        float time = 0f;
+
+        while (time < _duration)
+        {
+            float deltaTime = _useUnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
+            time += deltaTime;
+
+            float t = _duration > 0f ? time / _duration : 1f;
+            if (t > 1f)
+                t = 1f;
+
+            float scaleFactor = Mathf.Lerp(_startScale, _endScale, t);
+            _target.localScale = _baseScale * scaleFactor;
+
+            yield return null;
+        }
+
+        _target.localScale = _baseScale * _endScale;
+        _routine = null;
+    }
+}
